f1dc7307bde4760600f9e3b4b1b2108a4e9549b1
Erik Gagner <erik.gagner@yh.nackademin.se>
2022-11-15 14:13:44 +0100

    did the test exercises and now started on implementing workflow through github actions

---
+++ tests/test_assertions.py
@@ -0,0 +1,21 @@
+import pytest
+def test_equals():
+    assert 1 == 1
+## def test_equals_fails ():
+##    assert 1 == 2
+def test_greater_than():
+    assert 2 > 1
+def test_less_than():
+    assert 1 < 2
+def test_not_equal():
+    assert 1 != 2
+def test_greater_or_equal():
+    assert 2 >= 2
+def test_less_or_equal():
+    assert 1 <=2
+def test_equals_tuple():
+    assert (1,2) == (1,2)
+def test_exists_in():
+    assert 2 in [1,2,3]
+def test_same_string():
+    assert "abc" == "abc"
